d4ae12440c47a7d863c6b08adc3784894e27ae5d
holat <habeebabatunde9@gmail.com>
2022-09-08 16:06:02 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,17 +8,17 @@
 
 int main(void)
 {
-	char myCharacter;
-	int myInteger;
-	long myLong;
-	long long myLongLong;
-	float myFloat;
+	char acharacter;
+	int ainteger;
+	long along;
+	long long alonglong;
+	float afloat;
 
-	printf("Size of a char is %lu byte(s)\n", sizeof(myCharacter));
-	printf("Size of an int is %lu byte(s)\n", sizeof(myInteger));
-	printf("Size of a long int is %lu byte(s)\n", sizeof(myLong));
-	printf("Size of a long long int is %lu byte(s)\n", sizeof(myLongLong));
-	printf("Size of a float is %lu byte(s)\n", sizeof(myFloat));
+	printf("Size of a char: %lu byte(s)\n", sizeof(acharacter));
+	printf("Size of an int: %lu byte(s)\n", sizeof(ainteger));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(along));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(alonglong));
+	printf("Size of a float: %lu byte(s)\n", sizeof(afloat));
 
 	return (0);
 }
