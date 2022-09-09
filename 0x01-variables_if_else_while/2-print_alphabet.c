612a59ba066be6f88295f5516d572bf62d8976e0
holat <habeebabatunde9@gmail.com>
2022-09-09 16:29:02 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poiny
+ *
+ * Return - 0
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	for (; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
