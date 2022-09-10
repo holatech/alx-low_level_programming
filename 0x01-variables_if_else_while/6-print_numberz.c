d21c78e18d6cc485933dafc328af04a6336dd4c6
holat <habeebabatunde9@gmail.com>
2022-09-09 17:09:41 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main – Entry point
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	int a;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
