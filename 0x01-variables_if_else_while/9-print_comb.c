dc664b7f21d2cb25db6cf2b66bf7d421c8d9e3d8
holat <habeebabatunde9@gmail.com>
2022-09-09 17:51:55 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int a = '0';
+
+
+	while (a <= '9')
+	{
+		putchar(a);
+		if (a == '9')
+		{
+			putchar('\n');
+			return (0);
+		}
+		else
+		{
+			putchar(',');
+			putchar(' ');
+			a++;
+		}
+	}
+	return (0);
+}
