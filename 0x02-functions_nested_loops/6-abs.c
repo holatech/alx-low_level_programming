ad7870c0be9f18ffbb5aba1d895957278c097df2
holat <habeebabatunde9@gmail.com>
2022-09-13 16:18:41 -0700

    task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - check for absolute number
+ * @a: number
+ *
+ * Return: return a positive number
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	else
+	{
+		return (-a);
+	}
+}
