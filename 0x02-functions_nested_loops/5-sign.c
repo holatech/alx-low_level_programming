0abb7b5279793220f58807823c23c7ebf8b38bb1
holat <habeebabatunde9@gmail.com>
2022-09-13 16:10:49 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - checks number
+ * @n: number
+ * Return: 1 if n is +ve, 0 if n is zero or -1 if n is -ve
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
