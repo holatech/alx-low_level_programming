f3c21b601796eae4a6dae0a0180d310b028f1c17
holat <habeebabatunde9@gmail.com>
2022-09-15 16:35:27 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers - prints numbers
+ *
+ *Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+
+}
