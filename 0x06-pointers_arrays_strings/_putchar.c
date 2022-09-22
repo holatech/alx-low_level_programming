b27aa031b0d85f52e990d06ebd974789b9c9c918
holat <habeebabatunde9@gmail.com>
2022-09-22 15:05:08 -0700

    task 9

---
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _putchar - writes the character c to stdout
+ * @ch: the character argument
+ *
+ * Return: the character to be printed
+ *
+ */
+int _putchar(char ch)
+{
+	return (putchar(ch));
+}
