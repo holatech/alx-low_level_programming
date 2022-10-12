fb7a0e87b86efb89320c5fa373b8b709af5bd3c6
holat <habeebabatunde9@gmail.com>
2022-10-12 07:50:17 -0700

    task 4

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * op_sub -
+ *
+ * Return:
+ */
+int op_sub(int a, int b)
+{
+
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef HEADERGUARD
 #define HEADERGUARD
 
+
 void print_name(char *name, void (*f)(char *));
 int _putchar(char ch);
 void array_iterator(int *array, size_t size, void (*action)(int));
